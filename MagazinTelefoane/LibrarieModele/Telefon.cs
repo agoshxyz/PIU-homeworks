a85d931806d95c97040a3edaa03eec2ff2629c6b
agoshxyz <agageldidurdyyev@gmail.com>
2022-04-01 12:49:52 +0300

    lab3

---
+++ MagazinTelefoane/LibrarieModele/Telefon.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace LibrarieModele
+{
+    public class Telefon
+    {
+        //constante
+        private const char SEPARATOR_PRINCIPAL_FISIER = ';';
+
+        private const int ID = 0;
+        private const int TITLU = 1;
+        private const int DESCRIPTION = 2;
+        private const int PRET = 3;
+        private const int CANTITATE = 4;
+
+        //proprietati auto-implemented
+        private int idTelefon; //identificator unic telefon
+        private string titlu;
+        private string description;
+        private string pret;
+        private string cantitate;
+
+        //contructor implicit
+        public Telefon()
+        {
+            titlu = description = string.Empty;
+        }
+
+        //constructor cu parametri
+        public Telefon(int idTelefon, string titlu, string description, string pret, string cantitate)
+        {
+            this.idTelefon = idTelefon;
+            this.titlu = titlu;
+            this.description = description;
+            this.pret = pret;
+            this.cantitate = cantitate;
+        }
+
+        //constructor cu un singur parametru de tip string care reprezinta o linie dintr-un fisier text
+        public Telefon(string linieFisier)
+        {
+            var dateFisier = linieFisier.Split(SEPARATOR_PRINCIPAL_FISIER);
+
+            //ordinea de preluare a campurilor este data de ordinea in care au fost scrise in fisier prin apelul implicit al metodei ConversieLaSir_PentruFisier()
+            idTelefon = Convert.ToInt32(dateFisier[ID]);
+            titlu = dateFisier[TITLU];
+            description = dateFisier[DESCRIPTION];
+            pret = dateFisier[PRET];
+            cantitate = dateFisier[CANTITATE];
+
+        }
+
+        public string ConversieLaSir_PentruFisier()
+        {
+            string obiectTelefonPentruFisier = string.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}",
+                SEPARATOR_PRINCIPAL_FISIER,
+                idTelefon.ToString(),
+                (titlu ?? " NECUNOSCUT "),
+                (description ?? " NECUNOSCUT "),
+                (pret ?? " NECUNOSCUT "),
+                (cantitate ?? " NECUNOSCUT "));
+
+            return obiectTelefonPentruFisier;
+        }
+
+        public int GetIdTelefon()
+        {
+            return idTelefon;
+        }
+
+        public string GetTitlu()
+        {
+            return titlu;
+        }
+
+        public string GetDescription()
+        {
+            return description;
+        }
+
+        public string GetPret()
+        {
+            return pret;
+        }
+
+        public string GetCantitate()
+        {
+            return cantitate;
+        }
+
+        public void SetIdTelefon(int idTelefon)
+        {
+            this.idTelefon = idTelefon;
+        }
+    }
+}
---
+++ MagazinTelefoane/MagazinTelefoaneExemplu/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Configuration;
+using LibrarieModele;
+using NivelStocareDate;
+
+namespace EvidentaStudenti
+{
+    class Program
+    {
+        static void Main()
+        {
+            string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
+
+            AdministrareTelefoane_FisierText adminTelefoane = new AdministrareTelefoane_FisierText(numeFisier);
+            Telefon telefonNou = new Telefon();
+
+            int nrTelefoane = 0;
+
+            adminTelefoane.GetTelefoane(out nrTelefoane);
+
+
+            string optiune;
+            do
+            {
+                Console.WriteLine("C. Citire informatii telefoane de la tastatura");
+                Console.WriteLine("A. Afisarea ultimului telefonul introdus");
+                Console.WriteLine("F. Afisare telefonul din fisier");
+                Console.WriteLine("S. Salvare telefonul in fisier");
+                Console.WriteLine("X. Inchidere program");
+                Console.WriteLine("Alegeti o optiune");
+
+                optiune = Console.ReadLine();
+
+                switch (optiune.ToUpper())
+                {
+                    case "C":
+                        telefonNou = CitireTelefonTastatura();
+                        break;
+
+                    case "A":
+                        AfisareTelefon(telefonNou);
+                        break;
+
+                    case "F":
+                        Telefon[] studenti = adminTelefoane.GetTelefoane(out nrTelefoane);
+                        AfisareStudenti(studenti, nrTelefoane);
+                        break;
+
+                    case "S":
+                        int idTelefon = nrTelefoane + 1;
+                        telefonNou.SetIdTelefon(idTelefon);
+                        //adaugare telefon nou in fisier
+                        adminTelefoane.AddTelefon(telefonNou);
+                        nrTelefoane = nrTelefoane + 1;
+                        break;
+
+                    case "X":
+                        return;
+
+                    default:
+                        Console.WriteLine("Optiune nu exista");
+                        break;
+                }
+            } while (optiune.ToUpper() != "X");
+
+            Console.ReadKey();
+        }
+
+        public static void AfisareTelefon(Telefon telefon)
+        {
+            string infoTelefon = string.Format("Telefonul cu ID-ul #{0}\n are Titlul: {1} \n Descriptie: {2}\n Pretul: {3} \n Cantitate:{4} \n",
+                   telefon.GetIdTelefon(),
+                   telefon.GetTitlu() ?? " NECUNOSCUT ",
+                   telefon.GetDescription() ?? " NECUNOSCUT ",
+                   telefon.GetPret() ?? " NECUNOSCUT ",
+                   telefon.GetCantitate() ?? " NECUNOSCUT ");
+
+            Console.WriteLine(infoTelefon);
+        }
+
+        public static void AfisareStudenti(Telefon[] telefoane, int nrTelefoane)
+        {
+            Console.WriteLine("Telefoane sunt:");
+            for (int contor = 0; contor < nrTelefoane; contor++)
+            {
+                AfisareTelefon(telefoane[contor]);
+            }
+        }
+
+        public static Telefon CitireTelefonTastatura()
+        {
+            Console.WriteLine("Introduceti titlul: ");
+            string titlu = Console.ReadLine();
+
+            Console.WriteLine("Introduceti descriptie: ");
+            string description = Console.ReadLine();
+
+            Console.WriteLine("Introduceti pretul in lei: ");
+            string pret = Console.ReadLine();
+
+            Console.WriteLine("Introduceti cantitate: ");
+            string cantitate = Console.ReadLine();
+
+
+            Telefon telefon = new Telefon(0, titlu, description, pret, cantitate);
+
+            return telefon;
+        }
+    }
+}
